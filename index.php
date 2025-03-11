20b9e898292c11ea2afbf3827861e1defc0f1dd8
ahsan2649 <ahsan2649@outlook.com>
2025-03-12 00:05:25 +0100

    Episode 5 - Conditionals and Booleans

--- index.php
+++ index.php
@@ -3,14 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+            $message = "You have read $name.";
+        }
+        else {
+            $message = "You have not read $name.";
+        }
+    ?>
+
     <h1>
-        <?php
-            $greeting = "Hello";
-            // echo "$greeting" . " " . "Everybody!";
-            echo "$greeting Everybody!";
-        ?>
+        <?= $message; ?>
     </h1>
 </body>
 </html>
