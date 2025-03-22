7add73524b42747a89d54bfb613583a179318794
ahsan2649 <ahsan2649@outlook.com>
2025-03-22 22:00:53 +0100

    Episode 12 - Page Links

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
 
 
-$name = "Laracasts";
-$cost = 15;
-
-$business = [
-    "name" => "Laracasts",
-    "cost" => 15,
-    "categories" => ["Testing", "PHP", "JavaScript"]
-];
-
-if ($business["cost"] > 99){
-    echo "Not interested";
-}
-
-foreach ($business["categories"] as $category) {
-    echo $category;
-}
-
-function register($user){
-
-}
-
 require "index.view.php";
