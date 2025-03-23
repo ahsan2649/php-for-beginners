98975c78cfe23b860c8926e0557700f129ce31a3
ahsan2649 <ahsan2649@outlook.com>
2025-03-24 00:27:21 +0100

    Episode 22 - Render the Notes and Note Page

--- index.php
+++ index.php
@@ -2,17 +2,5 @@
 
 require "functions.php";
 require "Database.php";
+require "router.php";
 
-//require "router.php";
-
-$config = require "config.php";
-
-$db = new Database($config['database'], "root", "");
-
-$id = $_GET["id"];
-
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [":id" => $id])->fetchAll();
-
-dd($posts);
